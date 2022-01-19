92b3f0a79fc261ce3f5fa78d098e4b6063aa4ce5
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-01-19 15:01:40 -0300

    Novas Aulas

---
+++ JavaScript/Number/javascript/scripts.js
@@ -0,0 +1,18 @@
+/*
+números
+
+33 // Números inteiros
+12.5 // Números reais - float
+NaN // Not a number - Não é um números
+Infinity // infinito
+
+*/
+
+
+console.log(100); // Número inteiro
+
+console.log(9.5); // Número real
+
+console.log(10 / "asd"); // NaN não é um número (number + string)
+
+console.log(Infinity); // Infinito
---
+++ JavaScript/Object/javascript/scripts.js
@@ -0,0 +1,17 @@
+/*
+* Object
+    * objeto
+    * Propriedades / Atributos
+    * Funcionaliades / Métodos
+    
+  { propriedade: "valor" }
+
+*/
+
+console.log({
+    name: "Hiago",
+    idade: 17,
+    andar: function () {
+        console.log("andar")
+    }
+});
